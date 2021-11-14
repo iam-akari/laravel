a578db94b25e37d991d842f3df00ef1b1c30dcf5
iam-akari <haradaakari3845@gmail.com>
2021-11-14 17:08:10 +0900

    サイトバー追加

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,14 @@
+
+<header>
+  <script src="https://kit.fontawesome.com/901a2a9846.js" crossorigin="anonymous"></script>
+</header>
+<div class="list-group">
+  <a href="{{route('home')}}"
+  class="list-group-item {{url()->current()==route('home')?'active':""}}">
+    <i class="fas fa-home pr-2"></i><span>一覧表示</span>
+  </a>
+  <a href="{{route('post.create')}}"
+  class="list-group-item {{url()->current()==route('post.create')?'active':""}}">
+    <i class="fas fa-pen-nib pr-2"></i><span>新規投稿</span>
+  </a>
+</div>
