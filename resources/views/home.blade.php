1a1e3e3e483b13fe786d911131f561a4c1f39220
iam-akari <haradaakari3845@gmail.com>
2021-11-15 23:16:35 +0900

    コメント機能追加③

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,34 +1,62 @@
 @extends('layouts.app')
 @section('content')
-{{$user->name}}さん、こんにちは！
-
 @if(session('message'))
 <div class="alert alert-success">{{session('message')}}</div>
 @endif
-
-<div class="card-header">
-  一覧画面
+<div class="ml-2 mb-3">
+    <i class="fas fa-home pr-2"></i>home
 </div>
 @foreach ($posts as $post)
 <div class="container-fluid mt-20" style="margin-left:-10px;">
     <div class="row">
-        <div class="col-12">
+        <div class="col-md-12">
             <div class="card mb-4">
-
+                <div class="card-header">
                     <div class="media flex-wrap w-100 align-items-center">
                         <div class="media-body ml-3">
-                          <a href="{{route('post.show',$post)}}">{{ $post->title }}</a>
-                            <div class="text-muted small">{{ $post->user->name }}</div>
-
+                            {{-- ①件名 --}}
+                            <a href="{{route('post.show', $post)}}">{{ $post->title }}</a>
+                            <div class="text-muted small">
+                                {{-- ② 投稿者名 --}}
+                                {{ $post->user->name }}
+                            </div>
                         </div>
                         <div class="text-muted small ml-3">
                             <div>投稿日</div>
-                            <div><strong>{{$post->created_at->diffForHumans()}}</strong> </div>
+                            <div><strong>
+                                {{-- ③投稿作成日 --}}
+                                {{$post->created_at->diffForHumans()}}
+                            </strong>  </div>
                         </div>
+                    </div>
                 </div>
                 <div class="card-body">
-                    <p>{{$post->body}}</p>
+                    <p>
+                    {{-- ④本文 --}}
+                        {{ Str::limit ($post->body, 100, ' ...') }}
+                    </p>
                 </div>
+
+                <div class="card-footer d-flex flex-wrap justify-content-between align-items-center px-0 pt-0 pb-3">
+                    <div class="px-4 pt-3">
+                        @if ($post->comments->count())
+                        <span class="badge badge-success">
+                            返信 {{ $post->comments->count() }}件
+                        </span>
+                    @else
+                        <span>コメントはまだありません。</span>
+                    @endif
+
+                    </div>
+                    <div class="px-4 pt-3">
+                        <button type="button" class="btn btn-primary">
+                            <a href="{{route('post.show', $post)}}" style="color:white;">
+                                コメントする
+                            </a>
+                        </button>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
